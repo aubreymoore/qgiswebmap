a45482eff754365ee3f6f83d01efa12e9743a419
aubreymoore <aubreymoore@guam.net>
2020-07-02 10:30:07 +1000

    initial

---
+++ webmap/qgis2web_2020_07_02-10_13_02_056729/layers/images_2.js
@@ -0,0 +1,9 @@
+var json_images_2 = {
+"type": "FeatureCollection",
+"name": "images_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "frame": 0.0, "Longitude": 144.69243540968, "Latitude": 13.40477665897, "Photos": "<img src='..\/..\/20210630\/2021630_132859_0.jpg' width='768' height='432'>" }, "geometry": { "type": "Point", "coordinates": [ 144.692435409680002, 13.40477665897 ] } },
+{ "type": "Feature", "properties": { "frame": 1800.0, "Longitude": 144.68819014, "Latitude": 13.40343315, "Photos": "<img src='..\/..\/20210630\/2021630_132859_1800.jpg' width='768' height='432'>" }, "geometry": { "type": "Point", "coordinates": [ 144.688190139999989, 13.40343315 ] } }
+]
+}
---
+++ webmap/qgis2web_2020_07_02-10_13_02_056729/layers/layers.js
@@ -0,0 +1,54 @@
+var wms_layers = [];
+
+
+        var lyr_OpenStreetMap_0 = new ol.layer.Tile({
+            'title': 'OpenStreetMap',
+            'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+        });
+var format_2021630_132859_1 = new ol.format.GeoJSON();
+var features_2021630_132859_1 = format_2021630_132859_1.readFeatures(json_2021630_132859_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_2021630_132859_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_2021630_132859_1.addFeatures(features_2021630_132859_1);
+var lyr_2021630_132859_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_2021630_132859_1, 
+                style: style_2021630_132859_1,
+                interactive: true,
+                title: '<img src="styles/legend/2021630_132859_1.png" /> 2021630_132859'
+            });
+var format_images_2 = new ol.format.GeoJSON();
+var features_images_2 = format_images_2.readFeatures(json_images_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_images_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_images_2.addFeatures(features_images_2);
+var lyr_images_2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_images_2, 
+                style: style_images_2,
+                interactive: true,
+                title: '<img src="styles/legend/images_2.png" /> images'
+            });
+
+lyr_OpenStreetMap_0.setVisible(true);lyr_2021630_132859_1.setVisible(true);lyr_images_2.setVisible(true);
+var layersList = [lyr_OpenStreetMap_0,lyr_2021630_132859_1,lyr_images_2];
+lyr_2021630_132859_1.set('fieldAliases', {'stroke': 'stroke', 'stroke-width': 'stroke-width', 'stroke-opacity': 'stroke-opacity', });
+lyr_images_2.set('fieldAliases', {'frame': 'frame', 'Longitude': 'Longitude', 'Latitude': 'Latitude', 'Photos': 'Photos', });
+lyr_2021630_132859_1.set('fieldImages', {'stroke': 'TextEdit', 'stroke-width': 'Range', 'stroke-opacity': 'Range', });
+lyr_images_2.set('fieldImages', {'frame': 'Range', 'Longitude': 'TextEdit', 'Latitude': 'TextEdit', 'Photos': 'TextEdit', });
+lyr_2021630_132859_1.set('fieldLabels', {'stroke': 'no label', 'stroke-width': 'no label', 'stroke-opacity': 'no label', });
+lyr_images_2.set('fieldLabels', {'frame': 'no label', 'Longitude': 'no label', 'Latitude': 'no label', 'Photos': 'no label', });
+lyr_images_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
